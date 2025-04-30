624d1141a43af94d6dfe6bb685509c98b1ddafce
shaibi62 <146065158+shaibi62@users.noreply.github.com>
2025-04-30 17:29:11 +0500

    ss

--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -7,7 +7,6 @@ const DashboardPage = () => {
   return (
     <div style={{ padding: '20px', border: '2px solid green' }}>
       <h1>Dashboard</h1>
-      <p>If you see this, the basic Dashboard component and routing are working.</p>
     </div>
   );
 };
